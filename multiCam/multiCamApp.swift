44448400736ac5c8b22f968d57bfeb3855b8eea5
AngusEmmett <gusemmett@gmail.com>
2025-08-23 22:13:59 -0700

    Initial Commit

---
+++ multiCam/multiCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiCamApp.swift
+//  multiCam
+//
+//  Created by Angus Emmett on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct multiCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
